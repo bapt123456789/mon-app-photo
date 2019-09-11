4c1dd9fcae3a509887e33ee31fad1a510b814a3b
Baptiste <baptberthome@gmail.com>
2019-09-11 18:33:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+function click1() {
+	var btn1 = document.getElementById('btn1')
+	var btn2 = document.getElementById('btn2')
+	var btn3 = document.getElementById('btn3')
+	btn1.style.backgroundColor = "rgba(228, 77, 38, 0.15)";
+	btn2.style.backgroundColor = "#424548";
+	btn3.style.backgroundColor = "#424548";
+	var img1 = document.getElementById('img1').src = "img1.jpg"
+	var img2 = document.getElementById('img2').src = "img2.jpg"
+	var img3 = document.getElementById('img3').src = "img3.jpg"
+	var img4 = document.getElementById('img4').src = "img4.jpg"
+	var img5 = document.getElementById('img5').src = "img5.jpg"
+	var img6 = document.getElementById('img6').src = "img6.jpg"
+	var img7 = document.getElementById('img7').src = "img7.jpg"
+	var img8 = document.getElementById('img8').src = "img8.jpg"
+	var img9 = document.getElementById('img9').src = "img9.jpg"
+	var img10 = document.getElementById('img10').src = "img10.jpg"
+}
+
+function click2() {
+	var btn1 = document.getElementById('btn1')
+	var btn2 = document.getElementById('btn2')
+	var btn3 = document.getElementById('btn3')
+	btn1.style.backgroundColor = "#424548";
+	btn2.style.backgroundColor = "rgba(000, 112, 102, 0.15)";
+	btn3.style.backgroundColor = "#424548";
+	var img1 = document.getElementById('img1').src = "pano1.jpg"
+	var img2 = document.getElementById('img2').src = "pano2.jpg"
+	var img3 = document.getElementById('img3').src = "pano3.jpg"
+	var img4 = document.getElementById('img4').src = "pano4.jpg"
+	var img5 = document.getElementById('img5').src = "pano5.jpg"
+	var img6 = document.getElementById('img6').src = "pano6.jpg"
+	var img7 = document.getElementById('img7').src = "pano7.jpg"
+	var img8 = document.getElementById('img8').src = ""
+	var img9 = document.getElementById('img9').src = ""
+    var img10 = document.getElementById('img10').src = ""
+}
+
+function click3() {
+	var btn1 = document.getElementById('btn1')
+	var btn2 = document.getElementById('btn2')
+	var btn3 = document.getElementById('btn3')
+	btn1.style.backgroundColor = "#424548";
+	btn2.style.backgroundColor = "#424548";
+	btn3.style.backgroundColor = "rgba(241, 211, 79, 0.15)"
+	var img1 = document.getElementById('img1').src = ""
+	var img2 = document.getElementById('img2').src = ""
+	var img3 = document.getElementById('img3').src = ""
+	var img4 = document.getElementById('img4').src = ""
+	var img5 = document.getElementById('img5').src = ""
+	var img6 = document.getElementById('img6').src = ""
+	var img7 = document.getElementById('img7').src = ""
+	var img8 = document.getElementById('img8').src = ""
+	var img9 = document.getElementById('img9').src = ""
+	var img10 = document.getElementById('img10').src = ""
+}
